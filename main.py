947b7b6ce5ebd43ff1915685be76e9d8f5108a6e
ehcgnahc <jeter40906@gmail.com>
2025-03-04 01:03:12 +0800

    main 0.0.1

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
 
-request_url = "https://www.google.com"
+headers = {
+    "content-type": "text/html; charset=utf-8",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+}
 
-driver = webdriver.Chrome()
-driver.get(request_url)
-source = driver.page_source
-print(source)
-driver.quit()
+response = requests.get('https://csie.yuntech.edu.tw/index.php/newsclass', headers=headers)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+title_list = soup.select(".card-title a[title]")
+for news in title_list:
+    title = news["title"]
+    link = news["href"]
+    print(title, "https://csie.yuntech.edu.tw" + link)
