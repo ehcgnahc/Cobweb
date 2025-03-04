ec10c42a92c67fc4545bdaa9873880352c8619e2
ehcgnahc <jeter40906@gmail.com>
2025-03-04 11:12:46 +0000

    0.0.2

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import requests
+from requests import get
 from bs4 import BeautifulSoup
+import target
 
 headers = {
     "content-type": "text/html; charset=utf-8",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
 }
 
-response = requests.get('https://csie.yuntech.edu.tw/index.php/newsclass', headers=headers)
-soup = BeautifulSoup(response.text, 'html.parser')
+for site in target.sites:
+    print(site["school"])
+    response = get(site["url"], headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-title_list = soup.select(".card-title a[title]")
-for news in title_list:
-    title = news["title"]
-    link = news["href"]
-    print(title, "https://csie.yuntech.edu.tw" + link)
+    title_list = soup.select(".card-title a[title]")
+    for events in title_list:
+        title = events["title"]
+        link = events["href"]
+        print(title, "https://csie.yuntech.edu.tw" + link)
+    
+
---
+++ target.py
@@ -0,0 +1,4 @@
+sites = [
+    {"school": "NYUST", "url": "https://csie.yuntech.edu.tw/index.php/newsclass"},
+    {"school": "NCYU", "url": "https://website.ncyu.edu.tw/csie/"},
+]
